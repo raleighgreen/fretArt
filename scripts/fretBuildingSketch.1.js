71bf64d6f80b0480fbfef6bf6caee7008c18007b
Raleigh Green <raleighgreen@gmail.com>
2016-09-08 15:03:35 -0400

    First commit

---
+++ scripts/fretBuildingSketch.1.js
@@ -0,0 +1,111 @@
+var fretboard = {
+  highEstring: [
+    {note: 'E4', degree: '3', position: [300,200], audiofile: 'E4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F4', degree: '4', position: [300,200], audiofile: 'F4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#4', degree: '#4', position: [300,200], audiofile: 'F#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G4', degree: '5', position: [300,200], audiofile: 'G4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#4', degree: '#5', position: [300,200], audiofile: 'G#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A4', degree: '6', position: [300,200], audiofile: 'A4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#4', degree: 'b7', position: [300,200], audiofile: 'A#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'B4', degree: '7', position: [300,200], audiofile: 'B4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C5', degree: '1', position: [300,200], audiofile: 'C5 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#5', degree: 'b2', position: [300,200], audiofile: 'C#5 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D5', degree: '2', position: [300,200], audiofile: 'D5 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#5', degree: 'b3', position: [300,200], audiofile: 'D#5 note.mp3', ovalBody: 'true', dotBody: 'false'}
+
+  ],
+  Bstring: [
+    {note: 'B3', degree: '7', position: [300,200], audiofile: 'B3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C4', degree: '1', position: [300,200], audiofile: 'C4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#4', degree: 'b2', position: [300,200], audiofile: 'C#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D4', degree: '2', position: [300,200], audiofile: 'D4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#4', degree: 'b3', position: [300,200], audiofile: 'D#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'E4', degree: '3', position: [300,200], audiofile: 'E4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F4', degree: '4', position: [300,200], audiofile: 'F4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#4', degree: '#4', position: [300,200], audiofile: 'F#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G4', degree: '5', position: [300,200], audiofile: 'G4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#4', degree: '#5', position: [300,200], audiofile: 'G#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A4', degree: '6', position: [300,200], audiofile: 'A4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#4', degree: 'b6', position: [300,200], audiofile: 'A#4 note.mp3', ovalBody: 'true', dotBody: 'false'}
+  ],
+  Gstring: [
+    {note: 'G3', degree: '5', position: [300,200], audiofile: 'G3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#3', degree: '#5', position: [300,200], audiofile: 'G#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A3', degree: '6', position: [300,200], audiofile: 'A3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#3', degree: 'b7', position: [300,200], audiofile: 'A#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'B3', degree: '7', position: [300,200], audiofile: 'B3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C4', degree: '1', position: [300,200], audiofile: 'C4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#4', degree: 'b2', position: [300,200], audiofile: 'C#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D4', degree: '2', position: [300,200], audiofile: 'D4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#4', degree: 'b3', position: [300,200], audiofile: 'D#4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'E4', degree: '3', position: [300,200], audiofile: 'E4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F4', degree: '4', position: [300,200], audiofile: 'F4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#4', degree: '#4', position: [300,200], audiofile: 'F#4 note.mp3', ovalBody: 'true', dotBody: 'false'}
+  ],
+  Dstring: [
+    {note: 'D3', degree: '2', position: [300,200], audiofile: 'D3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#3', degree: 'b3', position: [300,200], audiofile: 'D#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'E3', degree: '3', position: [300,200], audiofile: 'E3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F3', degree: '4', position: [300,200], audiofile: 'F3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#3', degree: '#4', position: [300,200], audiofile: 'F#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G3', degree: '5', position: [300,200], audiofile: 'G3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#3', degree: '#5', position: [300,200], audiofile: 'G#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A3', degree: '6', position: [300,200], audiofile: 'A3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#3', degree: 'b7', position: [300,200], audiofile: 'A#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'B3', degree: '7', position: [300,200], audiofile: 'B3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C4', degree: '1', position: [300,200], audiofile: 'C4 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#4', degree: 'b2', position: [300,200], audiofile: 'C#4 note.mp3', ovalBody: 'true', dotBody: 'false'}
+  ],
+  Astring: [
+    {note: 'A2', degree: '6', position: [300,200], audiofile: 'A2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#2', degree: 'b7', position: [300,200], audiofile: 'A#2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'B2', degree: '7', position: [300,200], audiofile: 'B2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C3', degree: '1', position: [300,200], audiofile: 'C3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#3', degree: 'b2', position: [300,200], audiofile: 'C#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D3', degree: '2', position: [300,200], audiofile: 'D3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#3', degree: 'b3', position: [300,200], audiofile: 'D#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'E3', degree: '3', position: [300,200], audiofile: 'E3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F3', degree: '4', position: [300,200], audiofile: 'F3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#3', degree: '#4', position: [300,200], audiofile: 'F#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G3', degree: '5', position: [300,200], audiofile: 'G3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#3', degree: '#5', position: [300,200], audiofile: 'G#3 note.mp3', ovalBody: 'true', dotBody: 'false'}
+  ],
+  lowEstring: [
+    {note: 'E2', degree: '3', position: [300,200], audiofile: 'E2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F2', degree: '4', position: [300,200], audiofile: 'F2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'F#2', degree: '#4', position: [300,200], audiofile: 'F#2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G2', degree: '5', position: [300,200], audiofile: 'G2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'G#2', degree: '#5', position: [300,200], audiofile: 'G#2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A2', degree: '6', position: [300,200], audiofile: 'A2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'A#2', degree: 'b7', position: [300,200], audiofile: 'A#2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'B2', degree: '7', position: [300,200], audiofile: 'B2 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C3', degree: '1', position: [300,200], audiofile: 'C3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'C#3', degree: 'b2', position: [300,200], audiofile: 'C#3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D3', degree: '2', position: [300,200], audiofile: 'D3 note.mp3', ovalBody: 'true', dotBody: 'false'},
+    {note: 'D#3', degree: 'b3', position: [300,200], audiofile: 'D#3 note.mp3', ovalBody: 'true', dotBody: 'false'}
+  ]
+};
+
+// call the drawFretboard() function in the console to draw fretboard to screen
+var drawFretboard = function() {
+  var string1 = fretboard.highEstring;
+  var string2 = fretboard.Bstring;
+  var string3 = fretboard.Gstring;
+  var string4 = fretboard.Dstring;
+  var string5 = fretboard.Astring;
+  var string6 = fretboard.lowEstring;
+
+  var stringArray = [string1,string2,string3,string4,string5,string6];
+  var i=0;
+
+  document.write("<br>" + "--------------------------------------------------------------------------" + "<br>");
+  while (i < stringArray.length) {
+    for (var note in stringArray[i]) {
+      document.write(stringArray[i][note].note + ' | ');
+    };
+    document.write('<br>');
+    i++;
+  }
+  document.write("--------------------------------------------------------------------------" + "<br>");
+}
+drawFretboard();
