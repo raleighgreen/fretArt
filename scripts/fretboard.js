68fd2217a570b6b7a058a967239b85ee44d4b884
Raleigh Green <raleighgreen@gmail.com>
2016-09-14 17:57:23 -0400

    make array generate three guitars

--- scripts/fretboard.js
+++ scripts/fretboard.js
@@ -6,12 +6,16 @@ Code for video https://vimeo.com/channels/learningp5js/141919520
 var frets = [];
 
 function setup() {
+  debugger;
   createCanvas(600, 400);
-  for (var guitStr = 0; guitStr < 6; guitStr++){
-    for (var i = 0; i < 24; i++) {
-      var x = 80 + (i * 19);
-      var y = (guitStr * 19) + 70;
-      frets.push(new Fret(x, y));
+  for (var guitar = 0; guitar < 3; guitar++) {
+    var guitarCounter = guitar * 125;
+    for (var guitStr = 0; guitStr < 6; guitStr++){
+      for (var i = 0; i < 24; i++) {
+        var x = 80 + (i * 19);
+        var y = (guitStr * 19) + (guitarCounter + 30);
+        frets.push(new Fret(x, y));
+      }
     }
   }
 }
