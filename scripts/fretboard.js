9cd5bd65a18e3f03c810c02b1fdeace9123ee41c
Raleigh Green <raleighgreen@gmail.com>
2016-09-14 16:28:53 -0400

    add note toggle functionality

--- scripts/fretboard.js
+++ scripts/fretboard.js
@@ -7,10 +7,10 @@ var frets = [];
 
 function setup() {
   createCanvas(600, 400);
-  for (var guitNum = 0; guitNum < 6; guitNum++){
+  for (var guitStr = 0; guitStr < 6; guitStr++){
     for (var i = 0; i < 24; i++) {
       var x = 80 + (i * 19);
-      var y = (guitNum * 19) + 70;
+      var y = (guitStr * 19) + 70;
       frets.push(new Fret(x, y));
     }
   }
--- scripts/note.js
+++ scripts/note.js
@@ -1,18 +1,24 @@
 function Fret(x, y) {
   this.x = x;
   this.y = y;
+  this.active = false;
   this.col = color(93,81,214);
 
   this.display = function() {
     stroke(255);
+    if (this.active) {
+      this.col = color(68,238,203);
+    } else {
+      this.col = color(93,81,214);
+    }
     fill(this.col);
     ellipse(this.x, this.y, 17, 17);
-  }
+  };
 
   this.clicked = function() {
     var d = dist(mouseX, mouseY, this.x, this.y);
     if (d < 10) {
-      this.col = color(68,238,203);
+      this.active = !this.active;
     }
-  }
+  };
 }
