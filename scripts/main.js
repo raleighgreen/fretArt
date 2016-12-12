8443ebd23ad144d9a61836382112900bbc438849
Raleigh Green <raleighgreen@gmail.com>
2016-12-11 20:00:50 -0500

    added modes of Harmonic Minor

--- scripts/main.js
+++ scripts/main.js
@@ -18,6 +18,7 @@ var lydianLowestFrets = [402,324,246,168,90,12];
 var mixolydianLowestFrets = [401,324,246,168,89,11];
 var aeolienLowestFrets = [401,323,245,168,89,11];
 var locrianLowestFrets = [401,323,245,167,89,11];
+//---------------------
 var melMinLowestFrets = [402,323,246,168,89,12];
 var phrygianNat6LowestFrets = [401,323,246,167,89,11];
 var lydianAugmentedLowestFrets = [402,324,246,168,90,12];
@@ -25,7 +26,14 @@ var lydianb7LowestFrets = [401,324,246,168,90,11];
 var mixolydianb6LowestFrets = [401,324,245,168,89,11];
 var locrianNat2LowestFrets = [401,323,245,168,89,11];
 var alteredScaleLowestFrets = [401,323,245,167,88,11];
+//---------------------
 var harmMinLowestFrets = [402,323,245,168,89,12];
+var locrianNat6LowestFrets = [401,323,246,167,89,11];
+var ionianSharp5LowestFrets = [402,324,246,168,89,12];
+var dorianSharp4LowestFrets = [401,323,246,168,90,11];
+var phrygianDominantLowestFrets = [401,324,245,167,89,11];
+var lydianSharp2LowestFrets = [402,324,246,169,90,12];
+var alteredDominantbb7LowestFrets = [400,323,245,167,88,10];
 
 // Create modes and group them in an Object
 fretArt.modes = {
@@ -36,14 +44,22 @@ fretArt.modes = {
   mixolydian: new Mode("mixolydian", [2, 2, 1, 2, 2, 1, 2], mixolydianLowestFrets),
   aeolien: new Mode("Aeolien", [2, 1, 2, 2, 1, 2, 2], aeolienLowestFrets),
   locrian: new Mode("Locrian",[1, 2, 2, 1, 2, 2, 2], locrianLowestFrets),
+//---------------------
   melMin: new Mode("Melodic Minor",[2, 1, 2, 2, 2, 2, 1], melMinLowestFrets),
   phrygianNat6: new Mode("Phrygian Natural 6", [1, 2, 2, 2, 2, 1, 2], phrygianNat6LowestFrets),
   lydianAugmented: new Mode("Lydian Augmented",[2, 2, 2, 2, 1, 2, 1], lydianAugmentedLowestFrets),
   lydianb7: new Mode("Lydian b7", [2, 2, 2, 1, 2, 1, 2], lydianb7LowestFrets),
   mixolydianb6: new Mode("Mixolydian b6", [2, 2, 1, 2, 1, 2, 2], mixolydianb6LowestFrets),
   locrianNat2: new Mode("Locrian",[2, 1, 2, 1, 2, 2, 2], locrianNat2LowestFrets),
-  alteredScale: new Mode("Locrian",[1, 2, 1, 2, 2, 2, 2], alteredScaleLowestFrets),
-  harmMin: new Mode("Harmonic Minor",[2, 1, 2, 2, 1, 3, 1], harmMinLowestFrets)
+  alteredScale: new Mode("Altered Scale",[1, 2, 1, 2, 2, 2, 2], alteredScaleLowestFrets),
+//---------------------
+  harmMin: new Mode("Harmonic Minor",[2, 1, 2, 2, 1, 3, 1], harmMinLowestFrets),
+  locrianNat6: new Mode("Locrian Natural 6",[1, 2, 2, 1, 3, 1, 2], locrianNat6LowestFrets),
+  ionianSharp5: new Mode("Ionian #5",[2, 2, 1, 3, 1, 2, 1], ionianSharp5LowestFrets),
+  dorianSharp4: new Mode("Dorian #4",[2, 1, 3, 1, 2, 1, 2], dorianSharp4LowestFrets),
+  phrygianDominant: new Mode("Phrygian Dominant",[1, 3, 1, 2, 1, 2, 2], phrygianDominantLowestFrets),
+  lydianSharp2: new Mode("Lydian #2",[3, 1, 2, 1, 2, 2, 1], lydianSharp2LowestFrets),
+  alteredDominantbb7: new Mode("Altered Dominant bb7",[1, 2, 1, 2, 2, 1, 3], alteredDominantbb7LowestFrets)
 }
 
 // Create strings and group them in an array
