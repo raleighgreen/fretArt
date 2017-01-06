0446369b821870a14bcb19e93dc1590178fbce4d
Raleigh Green <raleighgreen@gmail.com>
2017-01-06 12:16:21 -0500

    key/scale arrow buttons now fully functional

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -25,8 +25,27 @@ arrowUp.addEventListener("mouseover", function() {
   arrowUp.style.transition = "opacity .1s";
   arrowUp.style.opacity = 1;
 });
+var scaleCounter = 0;
 arrowUp.addEventListener("click", function() {
-  console.log("arrowUp");
+  var scaleIndex = document.getElementById("scale-value");
+  // the scaleCounter 20 below needs to be hardcoded (its the id of the last
+  // option in #scale-value).
+  if (scaleIndex.selectedIndex == 0){
+    scaleCounter = 20;
+    document.getElementById(scaleCounter).selected = true;
+  } else {
+    scaleCounter -= 1;
+    document.getElementById(scaleCounter).selected = true;
+  }
+  processInput();
+  keyAndCurrentScaleDisplay()
+  for (var f = 0; f < fretArt.frets.length; f++) {
+    if (fretArt.frets[f].active) {
+      setScale(fretArt.currentKey, fretArt.currentMode.pattern);
+    }
+  }
+  isolateScaleIds(fretArt.foundScaleIds);
+  buildShapes();
 });
 arrowUp.addEventListener("mouseout", function() {
   arrowUp.style.transition = "opacity .1s";
@@ -38,7 +57,25 @@ arrowDown.addEventListener("mouseover", function() {
   arrowDown.style.opacity = 1;
 });
 arrowDown.addEventListener("click", function() {
-  console.log("arrowDown");
+  var scaleIndex = document.getElementById("scale-value");
+  // the number 22 below needs to be hardcoded (its the nuber of id'd options in
+  // #scale-value plus the number of disabled options).
+  if (scaleIndex.selectedIndex < 22){
+    scaleCounter += 1;
+    document.getElementById(scaleCounter).selected = true;
+  } else {
+    scaleCounter = 0;
+    document.getElementById(scaleCounter).selected = true;
+  }
+  processInput();
+  keyAndCurrentScaleDisplay()
+  for (var f = 0; f < fretArt.frets.length; f++) {
+    if (fretArt.frets[f].active) {
+      setScale(fretArt.currentKey, fretArt.currentMode.pattern);
+    }
+  }
+  isolateScaleIds(fretArt.foundScaleIds);
+  buildShapes();
 });
 arrowDown.addEventListener("mouseout", function() {
   arrowDown.style.transition = "opacity .1s";
