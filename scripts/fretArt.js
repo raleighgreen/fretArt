9ab2b6c8e2fa5fc9f3f26bc10262f18e3471e698
Raleigh Green <raleighgreen@gmail.com>
2017-03-07 14:40:41 -0500

    refined guitar body colo

--- scripts/fretArt.js
+++ scripts/fretArt.js
@@ -36,7 +36,7 @@ var fretArt = {
   endPoint: 0,
   noteSpacing: 25,
   stringSpacing: 20,
-  guitarBodyColor: [0,18,40],
+  guitarBodyColor: [10,25,70],
   guitarBodyDisplay: false,
   p5Canvas: null
 }
